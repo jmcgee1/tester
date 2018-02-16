42523086daeb24fae4b1b76a04fbbe431a043648
jmcgee1 <31488137+jmcgee1@users.noreply.github.com>
2018-02-16 16:59:23 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any kwy to exit.");
+            Console.ReadKey();
+        }
+    }
+}
